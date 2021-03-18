4df523444a822912a231993989320532455194ad
SimGus <simon.gustin@hotmail.com>
2021-03-18 20:48:00 +0100

    [doc] Explain in comments what service worker and foreground scripts are

--- foreground.js
+++ foreground.js
@@ -1,1 +1,7 @@
-console.log("This prints to the console of the page (inserted only if the page url matched)")
+// This script gets injected into any opened page
+// whose URL matches the pattern defined in the manifest
+// (see "content_script" key).
+// Several foreground scripts can be declared
+// and injected into the same or different pages.
+
+console.log("This prints to the console of the page (injected only if the page url matched)")
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
+// This is the service worker script, which executes in its own context
+// when the extension is installed or refreshed (or when you access its console).
+// It would correspond to the background script in chrome extensions v2.
+
 console.log("This prints to the console of the service worker (background script)")
