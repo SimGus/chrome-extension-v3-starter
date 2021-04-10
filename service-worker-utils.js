5ca3f99b4a1342ebdf363abc9a5365f28dad0445
SimGus <simon.gustin@hotmail.com>
2021-04-10 19:00:05 +0200

    [feat] Add external JS script loaded in the service worker + explanations

---
+++ service-worker-utils.js
@@ -0,0 +1,6 @@
+// This file can be imported inside the service worker,
+// which means all of its functions and variables will be accessible
+// inside the service worker.
+// The importation is done in the file `service-worker.js`.
+
+console.log("External file is also loaded!")
