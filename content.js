35b0d09b31713c00c675645a413534f0385f0134
Zachary Marion <zac.marion@coinbase.com>
2021-10-21 13:19:12 -0400

    Repro issue

--- content.js
+++ content.js
@@ -5,3 +5,5 @@
 // and injected into the same or different pages.
 
 console.log("This prints to the console of the page (injected only if the page url matched)")
+
+chrome.runtime.connect({ name: 'extensionUIRequest' });
