370a5bec13c97696436e70dcea6c01939ee487e7
Kosui Iwasa <ko.iwasa@kakehashi.life>
2023-04-25 15:51:10 +0900

    ブルーアーカイブ

--- foreground.js
+++
@@ -1,7 +0,0 @@
-// This script gets injected into any opened page
-// whose URL matches the pattern defined in the manifest
-// (see "content_script" key).
-// Several foreground scripts can be declared
-// and injected into the same or different pages.
-
-console.log("This prints to the console of the page (injected only if the page url matched)")
